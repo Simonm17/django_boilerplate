85a4e4088b1c84eb8ca0d9ac33772e76ba38b3f3
Ryan Chung <ryanchuung@gmail.com>
2020-10-22 04:26:20 -0700

    renamed config dir from social to config

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'social.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -80,7 +80,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'social.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     # 'default': {
